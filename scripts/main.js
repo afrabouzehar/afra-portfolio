2e41aaf299b6c84c288a6af90aece31c11fc1c1d
afrabouzehar <bouzehar.afra@gmail.com>
2026-03-01 13:40:17 +0000

    deleting a JS file

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-// Tiny enhancement: smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const id = link.getAttribute('href').slice(1);
-    const el = document.getElementById(id);
-    if (el) el.scrollIntoView({ behavior: 'smooth' });
-  });
-});
